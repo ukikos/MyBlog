2cf00c6d20326fd7101e5cd3d290f7206672a121
vakulindm01@mail.ru <vakulindm01@mail.ru>
2022-09-16 16:54:52 +0300

    update user mapper

--- src/main/java/com/vsu/myblog/mapper/UserMapper.java
+++ src/main/java/com/vsu/myblog/mapper/UserMapper.java
@@ -3,7 +3,10 @@ package com.vsu.myblog.mapper;
 import com.vsu.myblog.dto.user.UserCreateOrUpdateDto;
 import com.vsu.myblog.dto.user.UserDto;
 import com.vsu.myblog.model.entity.UserEntity;
+import org.mapstruct.BeanMapping;
 import org.mapstruct.Mapper;
+import org.mapstruct.MappingTarget;
+import org.mapstruct.NullValuePropertyMappingStrategy;
 
 @Mapper(uses = {RoleMapper.class, StatusMapper.class})
 public interface UserMapper {
@@ -15,4 +18,8 @@ public interface UserMapper {
     UserEntity toEntity(UserCreateOrUpdateDto requestDto);
 
     UserCreateOrUpdateDto toUserCreateOrUpdateDto(UserEntity entity);
+
+    @BeanMapping(nullValuePropertyMappingStrategy = NullValuePropertyMappingStrategy.IGNORE)
+    UserEntity updateEntity(@MappingTarget UserEntity target, UserCreateOrUpdateDto dto);
+
 }
